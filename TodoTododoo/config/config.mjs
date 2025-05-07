187209d735a2e568c9905bd4ffa1bfabca2d89e9
이솔비 <dmddnjs77@gmail.com>
2025-05-07 14:45:43 +0900

    재배포

--- TodoTododoo/config/config.mjs
+++ TodoTododoo/config/config.mjs
@@ -1,23 +1,16 @@
-//활경변수 env에서 불러와서 다른 파일들이 공통적으로 사용할 수 있도록 정리해서 내보내는 환경설정 전용 파일
-
-import dotenv from "dotenv"; // dot env 패키지 불러오기
-dotenv.config(); // env 파일 불러오기
-
 export const config = {
+  jwt: {
+    secretKey: process.env.JWT_SECRET,
+    expiresInSec: process.env.JWT_EXPIRES_SEC || "86400", // 기본 1일
+  },
+  bcrypt: {
+    saltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10, // 기본 10
+  },
   db: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE, // 사용할 데이터 베이스 이름
   },
-  jwt: {
-    secretKey: process.env.JWT_SECRET, // 토큰 암호화할 때 비밀키
-    expiresInSec: 86400, // 토큰 만료 시간 (초)
-  },
-
-  host: {
-    port: process.env.PORT || 5500, // 서버가 실행될 포트번호, 기본값 5000
-  },
+  port: process.env.PORT || 3000,
 };
-
-// process를 붙이는 이유 : 그냥 접근할 때 붙여야 함.
